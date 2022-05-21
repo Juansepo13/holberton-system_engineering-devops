f0ebc9e115ff236ab83654dc4b3f70f557259c21
Juansepo13 <juancho_s1314@hotmail.com>
2022-05-21 16:50:20 -0500

    act file 0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" number of subscribers in Reddit API """
+
+import requests
+
+def number_of_subscribers(subreddit):
+    """returns the number of subscribers"""
+    if subreddit is None or type(subreddit) is not str:
+        return 0
+    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
+                     headers={'User-Agent': 'Python/requests:APIproject:\
+v1.0.0 (by u/riskiebiznu)'}).json()
+    subs = r.get("data", {}).get("subscribers", 0)
+    return subs
